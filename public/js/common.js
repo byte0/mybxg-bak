c44469a3320b552cbe1f5c6364cef39a513dea3b
code1031 <wangzijie1031@qq.com>
2017-03-17 19:13:12 +0800

    登录功能完善

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,13 @@
 
-	NProgress.start();
+// 控制左侧导航菜单的显示和隐藏
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
 
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+// var pathname = location.pathname;
+// 没有登录的时候跳转到登录页面
+var flag = $.cookie('PHPSESSID');
+if(!flag){
+    // 没有登录
+    location.href = '/login'
+}
