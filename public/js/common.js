79af1854072c2dd30a838f450c79c8636c7e1245
code1031 <wangzijie1031@qq.com>
2017-03-18 17:40:15 +0800

    退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -38,10 +38,23 @@ define(['jquery','echarts','cookie'],function($,echarts){
         return false;//阻止默认行为
     });
 
+    // 退出功能
+    $('#logoutId').click(function(){
+        $.ajax({
+            type : 'post',
+            url : '/api/logout',
+            dataType : 'json',
+            success : function(data){
+                if(data.code == 200){
+                    location.href = '/login';
+                }
+            }
+        });
+    });
+
     // 渲染登录信息
     var obj = JSON.parse($.cookie('logInfo'));
     $('.aside .profile img').attr('src',obj.tc_avatar);
     $('.aside .profile h4').html(obj.tc_name);
-
     
 });
