a8ee411c90ce92ab8df4de2f00942e8bf531f794
code1031 <wangzijie1031@qq.com>
2017-03-18 17:33:58 +0800

    模块化重构

--- public/js/common.js
+++ public/js/common.js
@@ -1,13 +1,47 @@
 
-// 控制左侧导航菜单的显示和隐藏
-$('.navs ul').prev('a').on('click', function () {
-	$(this).next().slideToggle();
-});
+define(['jquery','echarts','cookie'],function($,echarts){
+    
+    // 控制左侧导航菜单的显示和隐藏
+    $('.navs ul').prev('a').on('click', function () {
+        $(this).next().slideToggle();
+    });
 
-var pathname = location.pathname;
-// 没有登录的时候跳转到登录页面
-var flag = $.cookie('PHPSESSID');
-if(!flag && pathname.indexOf('login') == -1){
-    // 没有登录
-    location.href = '/login'
-}
+    // 没有登录的时候跳转到登录页面
+    var pathname = location.pathname;
+    var flag = $.cookie('PHPSESSID');
+    if(!flag && pathname.indexOf('login') == -1){
+        // 没有登录
+        location.href = '/login'
+    }
+    // 实现登录功能
+    $('#loginForm').submit(function(){
+        var formData = $(this).serialize();
+        $.ajax({
+            type : 'post',
+            // url : 'http://api.studyit.com/login'
+            url : '/api/login',
+            data : formData,
+            dataType : 'json',
+            success : function(data){
+                if(data.code == 200){
+                    // console.log(data.result);
+                    var logInfo = JSON.stringify(data.result);
+                    // 实现cookie数据的跨页面共享
+                    $.cookie('logInfo',logInfo,{path : '/'});
+                    location.href = '/index/index'
+                }
+            },
+            error : function(data){
+                console.log(data.responseText);
+            }
+        });
+        return false;//阻止默认行为
+    });
+
+    // 渲染登录信息
+    var obj = JSON.parse($.cookie('logInfo'));
+    $('.aside .profile img').attr('src',obj.tc_avatar);
+    $('.aside .profile h4').html(obj.tc_name);
+
+    
+});
---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+
+
+require.config({
+    baseUrl : '/public',
+    paths : {
+        jquery : 'assets/jquery/jquery.min',
+        cookie : 'assets/jquery-cookie/jquery.cookie',
+        echarts : 'assets/echarts/echarts.min'
+    }
+});
