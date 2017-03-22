eda39311a3c7f08d0d3695c336b88300721165a0
code1031 <wangzijie1031@qq.com>
2017-03-22 11:50:18 +0800

    完成个人中心功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -13,14 +13,18 @@ define(['jquery','template','ckeditor','region','validate','form','datepicker','
             var html = template('profileTpl',data.result);
             $('#profileInfo').html(html);
             // 处理文件上传
-            // $('#upfile').uploadify({
-            //     fileObjName:'tc_avatar',
-            //     swf : '/public/assets/uploadify/uploadify.swf',
-            //     uploader : '/api/uploader/avatar',
-            //     onUploadSuccess : function(file,data){
-            //         console.log(data);
-            //     }
-            // });
+            $('#upfile').uploadify({
+                buttonText : '',
+                width : 120,
+                height : 120,
+                fileObjName:'tc_avatar',
+                swf : '/public/assets/uploadify/uploadify.swf',
+                uploader : '/api/uploader/avatar',
+                onUploadSuccess : function(file,data){
+                    data = JSON.parse(data);
+                    $('.preview img').attr('src',data.result.path);
+                }
+            });
 
             // 处理表单三级联动
             $('.hometown').region({
@@ -32,6 +36,33 @@ define(['jquery','template','ckeditor','region','validate','form','datepicker','
                 toolbarGroups : [
                     { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] }
                 ]
+            });
+            // 表单验证
+            $('#profileForm').validate({
+                sendForm : false,
+                valid : function(){
+                    // 同步更新富文本的内容变化
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    // 省市县信息
+                    var p = $('#p option:selected').text();
+                    var c = $('#c').find('option:selected').text();
+                    var d = $('#d').find('option:selected').text();
+                    var hometown = p + '|' + c + '|' + d;
+                    // 提交表单
+                    $(this).ajaxSubmit({
+                        type : 'post',
+                        url : '/api/teacher/modify',
+                        data : {tc_hometown : hometown},
+                        dataType : 'json',
+                        success : function(data){
+                            if(data.code == 200){
+                                location.href = '/index/settings';
+                            }
+                        }
+                    });
+                }
             });
         }
     });
